cf85389d2f328659f5e8d6bc3e2b8cb4e585d239
Lauri Katajisto <lauri.katajisto@geniem.com>
2018-07-25 16:46:42 +0300

    Get workouts from api.

--- src/app/workout.service.ts
+++ src/app/workout.service.ts
@@ -1,17 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Workout } from './workout';
-import { WORKOUTS } from './mock-workouts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkoutService {
+  private serviceUrl = 'https://evolve-fitness.herokuapp.com/workout';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   getWorkouts(): Observable<Workout[]> {
-    return of(WORKOUTS);
+    return this.http.get<Workout[]>(this.serviceUrl)
+    .pipe(
+      catchError(this.handleError('getWorkouts', []))
+    );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
